caff53f6f398534313c4a353478f6008f824a78e
JNAVAS <jose.navasordonez@gmail.com>
2024-06-05 00:03:51 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+
+import javax.swing.JFrame;
+import view.JFrame_Main;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+/**
+ *
+ * @author JOSE NAVAS
+ */
+public class Main {
+
+    public static void main(String[] args) {    
+        JFrame frame = new JFrame_Main();
+        frame.setSize(1060, 580);
+        frame.setVisible(true);
+        
+
+    }
+    
+}
